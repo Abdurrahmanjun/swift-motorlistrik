1dbb9485a1a9b31d63ba80c9469d4ea1e98fb21d
JUN <abdurrahmanjun@gmail.com>
2020-09-02 01:03:10 +0700

    Initial Commit

---
+++ MotorListrik/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MotorListrik
+//
+//  Created by Ericsson on 02/09/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
